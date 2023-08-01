66d43c43d43789f83bd6c3b1373c2a47f9d836e9
acolombo09 <acolombo0911@gmail.com>
2023-08-01 18:53:27 +0200

    completed exercise

--- js/main.js
+++ js/main.js
@@ -16,24 +16,32 @@ const app = createApp({
     };
   },
   methods: {
-    getEmailsFromApi() {},
   },
   mounted() {
     // lo inserisco qui perchè voglio che parta non appena si apre la pagina
     // se volessi farlo tramite un click, basta metterlo nei methods come solito
-    axios.get("https://flynn.boolean.careers/exercises/api/random/mail"
-    ).then((axiosResp) => {
+    for (let i = 0; i < 10; i++) {
+      axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((axiosResp) => {
+
       // dati inviati dal server come risposta alla richiesta fatta
-      axiosResp.data;
       // una volta ricevuti i dati dal server tramite axios, li salvo in randomEmails
       // dove data è l'oggetto che il server mi sta ritornando 
       // da quell' oggetto data, response è la chiave per accedere
-      this.randomEmails = axiosResp.data.response;
       /* ho provato con function normale ma non funziona in quanto il this è window
         serve il this di Vue, di conseguenza serve una arrow function 
-        scritta nel .then come callback function*/
-        
-    })
+        scritta nel .then come callback function
+      */
+
+      this.randomEmails.push(axiosResp.data.response);
+
+      // Verifica se ho ottenuto tutte le email
+      if (this.randomEmails.length === 10) {
+        // Ora posso procedere con il rendering dei risultati completi
+        console.log(this.randomEmails);
+      }
+    });
+    }
   },
 }).mount("#app");
 
